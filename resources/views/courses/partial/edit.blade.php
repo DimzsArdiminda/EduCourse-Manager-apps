f6bc699438bb296125f08271554c53f167603b9b
Dimas Ardiminda <113449449+DimzsArdiminda@users.noreply.github.com>
2025-01-22 11:23:40 +0700

    feat: profile

--- resources/views/courses/partial/edit.blade.php
+++ resources/views/courses/partial/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master-layout')
 
 @section('content')
-    <div class="p-6 text-gray-900">
+    <div class="p-6 text-gray-900 dark:text-gray-100">
         <b>
             <h1 class="text-3xl">Courses Data</h1>
         </b>
@@ -10,7 +10,7 @@
         <form action="{{ route('courses.update.bro') }}" method="POST">
             @csrf
             <div class="grid grid-cols-1 gap-4 mt-8 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1">
-                <div class="p-4 bg-white shadow-md rounded-lg">
+                <div class="p-4 dark:bg-gray-700 dark:text-gray-200 shadow-md rounded-lg">
                     <div class="mb-5 mt-5">
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
                         <input type="text" name="name" required id="name" value="{{ $courses->Nama_kursus }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -36,7 +36,7 @@
                 {{-- button --}}
                 <div class="flex justify-end">
                     <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        update Course
+                        Update Course
                     </button>
                 </div>
             </div>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,24 +1,50 @@
 @extends('layouts.master-layout')
 @section('content')
 
-    <div class="py-12">
+
+   <div class="p-6 text-gray-900 dark:text-gray-100">
+        <b>
+            <h1 class="text-3xl">Hai, {{ Auth::user()->name }} 👋</h1>
+        </b>
+        <p class="mt-4">Welcome to your profile. <br>
+            Here you can update your profile information, change your password, or delete your account.
+        </p>
+
+        {{-- layout --}}
+        <div class="grid grid-cols-1 gap-4 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+            <button class="btn-toggle-form" data-target="form-update-profile">
+                <div class="p-4 dark:bg-gray-700 dark:text-gray-200 shadow-md rounded-lg">
+                    <h5 class="text-lg font-semibold">Update Profile</h5>
+                        Open
+                </div>
+            </button>
+            <button class="btn-toggle-form" data-target="form-update-password">
+                <div class="p-4 dark:bg-gray-700 dark:text-gray-200 shadow-md rounded-lg">
+                    <h2 class="text-lg font-semibold">Update Password</h2>
+                        Open
+                </div>
+            </button>
+            <button class="btn-toggle-form" data-target="form-delete-account">
+                <div class="p-4 dark:bg-gray-700 dark:text-gray-200 shadow-md rounded-lg">
+                    <h2 class="text-lg font-semibold">Delete Account</h2>
+                        Open
+                    </div>
+            </button>
+        </div>
+        
+    </div>
+    <div class="py-12 ">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
+            <div id="form-update-profile" class="form-section hidden">
+                @include('profile.partials.update-profile-information-form')
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
+            <div id="form-update-password" class="form-section hidden">
+                @include('profile.partials.update-password-form')
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
+            <div id="form-delete-account" class="form-section hidden">
+                @include('profile.partials.delete-user-form')
             </div>
         </div>
     </div>
